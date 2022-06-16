64bd73504da534f84cdb4871d1a4d43e81dcabf8
David OGIEVA <davidogieva32@gmail.com>
2022-06-16 13:40:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let loginForm = document.querySelector('.login-form-container');
+let loginBtn = document.querySelector('#login-btn');
+let closeForm = document.querySelector('#form-close');
+let menuBar = document.querySelector('#menu-bar');
+let navBar = document.querySelector('.navbar');
+let videoBtn = document.querySelectorAll('.vid-btn'); 
+
+
+window.onscroll = ()=> {
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+    menuBar.classList.remove('fa-times');
+    navBar.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+menuBar.addEventListener('click', ()=> {
+    menuBar.classList.toggle('fa-times');
+    navBar.classList.toggle('active');
+})
+
+
+searchBtn.addEventListener('click', ()=> {
+    searchBtn.classList.toggle('fa-times')
+    searchBar.classList.toggle('active')
+});
+
+loginBtn.addEventListener('click', ()=> {
+    loginForm.classList.add('active');
+});
+
+closeForm.addEventListener('click', ()=> {
+    loginForm.classList.remove('active');
+});
+
+videoBtn.forEach(btn => {
+    btn.addEventListener('click', ()=> {
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src
+    })
+});
